d5ee7e3248a830a3ff121b9beeaa68ccead13ca0
Shulgin Roman <roman@shulgin.pro>
2023-05-12 23:27:20 +0300

    react-app was created

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import '@geoman-io/leaflet-geoman-free';
+import '@geoman-io/leaflet-geoman-free/dist/leaflet-geoman.css';
+import React from 'react';
+
 const tiles1 = L.tileLayer(
-  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-  {
-    maxZoom: 19,
-    attribution:
-      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-  }
+    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    {
+      maxZoom: 19,
+      attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }
 );
 
 
-const map2 = L.map('example2').setView([31.771959, 35.217018], 8).addLayer(tiles1);
+const map2 = L.map('map_root').setView([31.771959, 35.217018], 8).addLayer(tiles1);
 const bounds = L.latLngBounds(
-  L.latLng(29.487, 34.245), // Southwestern point of Israel
-  L.latLng(33.345, 35.836)  // Northeastern point of Israel
+    L.latLng(29.487, 34.245), // Southwestern point of Israel
+    L.latLng(33.345, 35.836)  // Northeastern point of Israel
 );
 
 map2.fitBounds(bounds);
@@ -29,8 +35,8 @@ map2.pm.addControls({
 
 map2.pm.disableDraw('Polygon');
 map2.pm.enableDraw('Circle', {
-    snappable: true,
-    cursorMarker: true
+  snappable: true,
+  cursorMarker: true
 });
 
 map2.pm.enableDraw('Line', { allowSelfIntersection: false });
@@ -69,43 +75,13 @@ if(geoJsonData) {
   });
 
 }
-map2.on('pm:drawstart', function (e) {
-  const layer = e.workingLayer;
-  // console.log(layer);
-  layer.on('pm:centerplaced', function (e) {
-    // console.log(e);
-  });
-});
-map2.on('pm:create', function (e) {
-  const layer = e.layer;
-  // console.log(layer);
-  layer.on('pm:centerplaced', function (e) {
-    // console.log(e);
-  });
-});
 
 map2.pm.enableDraw('Polygon', { allowSelfIntersection: false });
 map2.pm.disableDraw('Polygon');
 map2.pm.enableDraw('Line', { allowSelfIntersection: false });
 map2.pm.disableDraw('Line');
 
-
-function saveToStorageSendData() {
-  const res = [];
-  for(const key in map2._layers) {
-    const layer = map2._layers[key]
-    if(layer.toGeoJSON && layer.toGeoJSON().geometry) {
-      res.push(treatCircle(layer.toGeoJSON(), layer));
-    }
-  }
-  localStorage.areas = res.length
-    ? JSON.stringify({
-      type: 'FeatureCollection',
-      features: res })
-    : ''
-  console.log('data ready for sending::', res);
-}
-function treatCircle(geoJson, layer) {
+function treatCircle(geoJson: any, layer: any) {
   if(geoJson.geometry.type === 'Point') {
     geoJson.properties = {
       customGeometry: {
@@ -116,3 +92,29 @@ function treatCircle(geoJson, layer) {
   return geoJson
 }
 
+function saveToStorageSendData() {
+  const res = [];
+  const layers = (map2 as any)._layers
+  for(const key in layers) {
+    const layer = layers[key]
+    if(layer.toGeoJSON && layer.toGeoJSON().geometry) {
+      res.push(treatCircle(layer.toGeoJSON(), layer));
+    }
+  }
+  localStorage.areas = res.length
+      ? JSON.stringify({
+        type: 'FeatureCollection',
+        features: res })
+      : ''
+  console.log('data ready for sending::', res);
+}
+
+function App() {
+  return (
+    <div className="App">
+      <button onClick={saveToStorageSendData} className="save-button">Save</button>
+    </div>
+  );
+}
+
+export default App;
