610b954abfcae8f29f6f4b20f4f58280538fb8e1
Shulgin Roman <roman@shulgin.pro>
2023-05-26 17:12:39 +0300

    fix cloud functions

--- functions/collections/[hash].ts
+++ functions/collections/[hash].ts
@@ -1,26 +1,36 @@
 interface Env {
     COLLECTION_URL: string;
 }
-export const onRequestGet: PagesFunction<Env> = ({env, params}) => {
+export const onRequestGet: PagesFunction<Env> = async ({env, params}) => {
     try {
         const hash = params.hash;
-        return hash
-            ? fetch(env.COLLECTION_URL+ "/" + hash)
-            : new Response("That's not a valid hash.");
+        if (hash) {
+            const response = await fetch(env.COLLECTION_URL+ "/" + hash);
+            return new Response(await response.text(), { status: response.status });
+        } else {
+            return new Response("That's not a valid hash.", { status: 400 });
+        }
     } catch(err: any) {
-        return new Response(err.stack, { status: 500 })
+        console.error(err.stack);
+        return new Response("An error occurred.", { status: 500 })
     }
 }
 
-export const onRequestPost: PagesFunction<Env> = ({env, params, data}) => {
+export const onRequestPost: PagesFunction<Env> = async ({env, params, data}) => {
     try {
         const hash = params.hash;
-
-        return hash
-            ? fetch(env.COLLECTION_URL + "/" + hash, {method: 'POST', body: JSON.stringify(data)})
-            : new Response("That's not a valid hash.");
-
+        if (hash) {
+            const response = await fetch(env.COLLECTION_URL + "/" + hash, {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify(data)
+            });
+            return new Response(await response.text(), { status: response.status });
+        } else {
+            return new Response("That's not a valid hash.", { status: 400 });
+        }
     } catch(err: any) {
-        return new Response(err.stack, { status: 500 })
+        console.error(err.stack);
+        return new Response("An error occurred.", { status: 500 })
     }
 }
